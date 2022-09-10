dc271525062968178d04dd70b1b89d01050d1fa1
Deramijoe <ogboledoh4@gmail.com>
2022-09-10 05:59:00 -0700

    Task 5, proper grammer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function 
+ * this program prints "some statements in the correct grammer"
+ * Return 0
+ */
+int main(void)
+{
+	printf("with a proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
